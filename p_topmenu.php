d3041d882adae68bf7e5a86dcc47596b3517a80b
Giuseppe Siciliano <Giuseppe Siciliano>
2017-02-16 12:52:16 +0100

    added new font

--- p_topmenu.php
+++ p_topmenu.php
@@ -1,8 +1,10 @@
 
 <div class="top-menu">
+
     <div class="col-sm-4">
-        <h2><img class="top-logo" src="img/howlout_icon_with_border.png" >&nbsp;&nbsp;HowlOut</h2>
+        <img class="top-logo" src="img/howlout_icon_with_border.png" ><span class="top-menu-headertext">HowlOut</span>
     </div>
+
     <div class="col-sm-8">
         <span style="margin-left:70px;">Search&nbsp;&nbsp;</span>
         <input class="top-menu-searchbar" type="text" placeholder="Quick search...">
