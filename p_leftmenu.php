d447b208acb79421710032c16552e7a11213cd47
Jonas_P <jonas.paul.persson@gmail.com>
2017-02-16 14:30:48 +0100

    Added initial left menu

---
+++ p_leftmenu.php
@@ -0,0 +1,32 @@
+<link href="css/p_leftmenu.css" rel="stylesheet">
+<div class="leftmenu">
+	<div>
+		<a href="conversations.php"><i class="fa fa-comment-o leftmenuitem"></i>My events</a>
+	</div>
+	<br>
+	<div class="leftmenugroup">
+		Events
+	</div>
+	<div>
+		<a href="event.php"><i class="material-icons leftmenuitem">event</i>My events</a>
+	</div>
+	<div>
+		<a href="event.php"><i class="material-icons leftmenuitem">event</i>Discover events</a>
+	</div>
+	<div>
+		<a href="event.php"><i class="material-icons leftmenuitem">event</i>Create event</a>
+	</div>
+	<div>
+		<a href="event.php"><i class="material-icons leftmenuitem">event</i>Event manager</a>
+	</div>
+	<br>
+	<div class="leftmenugroup">
+		Groups
+	</div>
+	<div>
+		<a href="event.php"><i class="material-icons leftmenuitem">group_work</i>My groups</a>
+	</div>
+	<div>
+		<a href="event.php"><i class="material-icons leftmenuitem">group_work</i>Create group</a>
+	</div>
+</div>
