3f97f49c2f7981cbb0f0be4a9d6236f427629825
Jonas_P <jonas.paul.persson@gmail.com>
2017-02-16 15:39:22 +0100

    More work on left menu

--- p_leftmenu.php
+++ p_leftmenu.php
@@ -1,32 +1,33 @@
 <link href="css/p_leftmenu.css" rel="stylesheet">
 <div class="leftmenu">
 	<div>
-		<a href="conversations.php"><i class="fa fa-comment-o leftmenuitem"></i>My events</a>
+		<a href="conversations.php"><i class="material-icons leftmenuitem">chat</i>Conversations</a>
+		<!-- <i class="fa fa-comment-o leftmenuitem convicon"> -->
 	</div>
 	<br>
 	<div class="leftmenugroup">
 		Events
 	</div>
 	<div>
-		<a href="event.php"><i class="material-icons leftmenuitem">event</i>My events</a>
+		<a href="event.php"><i class="material-icons leftmenuitem">event_note</i>My events</a>
 	</div>
 	<div>
-		<a href="event.php"><i class="material-icons leftmenuitem">event</i>Discover events</a>
+		<a href="event.php"><i class="material-icons leftmenuitem">event_available</i>Discover events</a>
 	</div>
 	<div>
 		<a href="event.php"><i class="material-icons leftmenuitem">event</i>Create event</a>
 	</div>
 	<div>
-		<a href="event.php"><i class="material-icons leftmenuitem">event</i>Event manager</a>
+		<a href="event.php"><i class="material-icons leftmenuitem">event_busy</i>Event manager</a>
 	</div>
 	<br>
 	<div class="leftmenugroup">
 		Groups
 	</div>
 	<div>
-		<a href="event.php"><i class="material-icons leftmenuitem">group_work</i>My groups</a>
+		<a href="event.php"><i class="material-icons leftmenuitem">group</i>My groups</a>
 	</div>
 	<div>
-		<a href="event.php"><i class="material-icons leftmenuitem">group_work</i>Create group</a>
+		<a href="event.php"><i class="material-icons leftmenuitem">group_add</i>Create group</a>
 	</div>
 </div>
