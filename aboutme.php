5018d5399a78e0797502f432e07ca6c748e6ca57
Giuseppe Siciliano <Giuseppe Siciliano>
2017-04-18 15:51:15 +0200

    about me page, groups fixed

--- aboutme.php
+++ aboutme.php
@@ -76,16 +76,13 @@ session_start();
 
                 <h4 style="margin-top:50px;"><i class="material-icons icon_peep" aria-hidden="true" style="font-size:26px;vertical-align:middle;">group</i>&nbsp;&nbsp;My groups</h4>
                 <hr>
-                <div class="row">
-                    <div class="group-circle col-md-1" style="background-image: url('img/howlout_icon.png');background-size:100%;margin:0 30px 0 30px;"></div>
-                    <div class="group-circle col-md-1" style="background-image: url('img/howlout_icon.png');background-size:100%;margin:0 30px 0 30px;"></div>
-                    <div class="group-circle col-md-1" style="background-image: url('img/howlout_icon.png');background-size:100%;margin:0 30px 0 30px;"></div>
-                    <div class="group-circle col-md-1" style="background-image: url('img/howlout_icon.png');background-size:100%;margin:0 30px 0 30px;"></div>
+                <div class="groupBox">
+
                 </div>
 
 
                 <br>
-                <a href="" style="float:right;font-size:14px;margin-top:10px;">View all</a>
+                <a href="mygroups.php" style="float:right;font-size:14px;">View all</a>
                 <!--      PAGE CONTENT GOES HERE      -->
             </div>
         </div>
@@ -99,11 +96,69 @@ session_start();
     <?php include_once "p_footer.html"; ?>
 
     <?php include_once "p_loadScripts.html"; ?>
-
+    <script src="js/grouphandler.js"></script>
     <script>
         $(".btnChangePicture").click(function() {
             $(".fileChangePicture").click();
         });
+
+        // Get groups
+        var facebookId = "";
+        window.fbAsyncInit = function() {
+            // facebook functions in here
+            FB.init({
+                appId      : '1897963557117405',
+                xfbml      : true,
+                version    : 'v2.8'
+            });
+            FB.AppEvents.logPageView();
+
+
+            // Get groups
+            FB.getLoginStatus(function(response) {
+                FB.api('/me', function(response)
+                {
+                    facebookId = response.id;
+                    var apiLink2 = 'https://api.howlout.net/profile/'+facebookId;
+                    var apiData = JSON.stringify(
+                        {
+                            ProfileId : facebookId
+                        }
+                    );
+                    var token = $(".token").data("token");
+                    $.ajax({
+                        type: 'post',
+                        url: '_apiRequest.php',
+                        async: false,
+                        data: {'apiLink' : apiLink2, 'apiData' : apiData, 'token' : token},
+                        success: function (data) {
+                            var jsonData = JSON.parse(data);
+                            console.log(data);
+                            var counter = 1;
+                            $.each(jsonData.Groups, function(i,ele) {
+                                console.log(ele);
+                                if (counter<=6) {
+                                    $(".groupBox").append(makeGroupElement(ele));
+                                    counter++
+                                }
+                            });
+                        },
+                        error: function () {
+                            alert("ajax failed");
+                        }
+                    });
+                });
+            });
+
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
     </script>
 </body>
 
